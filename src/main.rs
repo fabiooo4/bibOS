872514e3c0e0c455de6b125fa174a0b826766acd
fabiooo4 <fabioirimie@gmail.com>
2025-04-20 23:44:32 +0200

    hello world VGA

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,22 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello, World!";
+
 // Overwrite the default entry point function
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    panic!();
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        let i: isize = i as isize * 2;
+        unsafe {
+            *vga_buffer.offset(i) = byte;
+            *vga_buffer.offset(i + 1) = 0xb;
+        }
+    }
+
+    panic!()
 }
 
 // Define a custom panic handler that does not return
